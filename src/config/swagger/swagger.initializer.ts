17c33299319caafe359745cf8a77628826efa33d
Emanuel Canavesio <canavesio.em@protonmail.com>
2023-07-14 02:17:11 -0300

    complete skeleton and initial configuration of project

---
+++ src/config/swagger/swagger.initializer.ts
@@ -0,0 +1,28 @@
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+/**
+ * @TODOS
+ * - Create a tags enum
+ * - Complete descriptions on each tag
+ * - Complete descriptions of documentation
+ */
+export abstract class SwaggerInitializer {
+  public static run(app: NestExpressApplication): void {
+    const config = new DocumentBuilder()
+      .setTitle("Taxi 24")
+      .setDescription("")
+      .setVersion("1.0")
+      .addTag("Drivers")
+      .addTag("Passengers")
+      .addTag("Trips")
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document, {
+      customfavIcon: "/assets/logo.png",
+      customCssUrl: "/assets/styles.css",
+      customSiteTitle: "Taxi24 - Swagger UI",
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import helmet from "helmet";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import { AppModule } from "./app.module";
+import { SwaggerInitializer } from "./config/swagger/swagger.initializer";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+    credentials: true,
+  });
+  app.enableShutdownHooks();
+  app.use(helmet());
+  app.disable("X-Powered-By");
+
+  SwaggerInitializer.run(app);
+
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
 }
 bootstrap();
